70e102715d656daa9af55a10cb829c157dfa400b
DenysLiashenko <liashenko.denys.romanovych@gmail.com>
2024-04-22 17:09:16 +0200

    1st

---
+++ lab14_Ляшенко/lab14_Ляшенко.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+#include "windows.h"
+
+void main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	const int array_size = 17;
+	int array[array_size] = { 3, 4, 6, 8, 12, 15, 18, 20, 25, 33, 35, 39, 45, 49, 57, 60, 62 }; // оголошення одновимірного відсортованого масиву
+	int search_num; // змінна для зберігання шуканого в масиві елемента
+		cout << "Введіть елемент, який необхідно знайти в масиві"<<endl;
+		cin >> search_num;
+
+	for (int i = 0; i < array_size; i++)
+	{
+		cout << array[i] << " "; //виведення масиву
+	}
+	cout << endl;
+
+	//інтерполяційний пошук
+	int mid, left = 0, right = array_size - 1;
+	while (array[left] <= search_num && array[right] >= search_num)
+	{
+		mid = left + ((search_num - array[left]) * (right - left)) /
+			(array[right] - array[left]);
+		if (array[mid] < search_num)
+		{
+			left = mid + 1;
+		}
+		else if (array[mid] > search_num)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			cout << "Елемент " << search_num << " має індекс " << mid
+				<< endl;
+			break;
+		}
+	}
+	if (!(array[left] <= search_num && array[right] >= search_num))
+	{
+		cout << "Елемента немає в масиві";
+	}
+}
---
+++ Завдання 1/Завдання 1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int search(int array[25], int search_num)
+{
+	int mid, left = 0, right = 25 - 1;
+	while (array[left] <= search_num && array[right] >= search_num)
+	{
+		mid = left + ((search_num - array[left]) * (right - left)) /
+			(array[right] - array[left]);
+		if (array[mid] < search_num)
+		{
+			left = mid + 1;
+		}
+		else if (array[mid] > search_num)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			return mid;
+			break;
+		}
+	}
+
+}
+
+int main()
+{
+	int search_num, pos,  array[] = { 1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,77,88,99,101,111,121,131,141,151,161};
+	cout << "Enter the number that you are searching: ";
+	cin >> search_num;
+
+	pos = search(array,search_num);
+
+	cout << "Number  " << search_num << " is on " << pos << " position" << endl;
+}
